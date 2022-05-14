671732ac73496aa2b6d15c4d7e18afec83c214c4
JulienC-Dev <julien.cormier@yahoo.fr>
2022-05-14 15:30:55 +0200

    refactorisation flake8

--- lettings/tests/conftest.py
+++ lettings/tests/conftest.py
@@ -5,7 +5,6 @@ from lettings.models import Address, Letting
 @pytest.fixture(scope='module')
 def django_db_setup(django_db_setup, django_db_blocker):
     with django_db_blocker.unblock():
-        address = Address.objects.create(number=300, street="rue de pierre", city="chelles", state="FR", zip_code=77500,
-                         country_iso_code="FRA")
+        address = Address.objects.create(number=300, street="rue de pierre", city="chelles",
+                                         state="FR", zip_code=77500, country_iso_code="FRA")
         Letting.objects.create(title="Location du soleil", address=address)
-
--- profiles/tests/conftest.py
+++ profiles/tests/conftest.py
@@ -6,5 +6,6 @@ from django.contrib.auth.models import User
 @pytest.fixture(scope='module')
 def django_db_setup(django_db_setup, django_db_blocker):
     with django_db_blocker.unblock():
-        user = User.objects.create_user(username="paul", email="paul@yahoo.fr", password="test321321")
+        user = User.objects.create_user(username="paul", email="paul@yahoo.fr",
+                                        password="test321321")
         Profile.objects.create(user=user, favorite_city="Paris")
