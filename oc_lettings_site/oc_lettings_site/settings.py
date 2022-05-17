d10bbf6b5f7884ad790202c92b3c3a699ef1b5f4
JulienC-Dev <julien.cormier@yahoo.fr>
2022-05-17 14:42:16 +0200

    modif

--- oc_lettings_site/oc_lettings_site/settings.py
+++ oc_lettings_site/oc_lettings_site/settings.py
@@ -6,7 +6,7 @@ load_dotenv()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
