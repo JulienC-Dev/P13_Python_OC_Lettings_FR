b407debc7f965dd55f1a48968e77994fe1247ba9
JulienC-Dev <julien.cormier@yahoo.fr>
2022-05-17 20:23:56 +0200

    add sentry

--- oc_lettings_site/oc_lettings_site/urls.py
+++ oc_lettings_site/oc_lettings_site/urls.py
@@ -3,14 +3,10 @@ from django.contrib import admin
 from . import views
 
 
-def trigger_error(request):
-    division_by_zero = 1 / 0
-
-
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
-    path('sentry-debug/', trigger_error),
+    path('sentry-debug/', views.trigger_error),
     path('lettings/', include('lettings.urls')),
     path('profiles/', include('profiles.urls')),
 ]
--- oc_lettings_site/oc_lettings_site/views.py
+++ oc_lettings_site/oc_lettings_site/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+    return division_by_zero
 # Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie quam lobortis
 # leo consectetur ullamcorper non id est. Praesent dictum, nulla eget feugiat sagittis, sem
 # mi convallis eros,
@@ -8,5 +11,7 @@ from django.shortcuts import render
 # lobortis quis. Phasellus eleifend ex auctor venenatis tempus. Aliquam vitae erat ac orci
 # placerat luctus. Nullam elementum urna nisi, pellentesque iaculis enim cursus in. Praesent
 # volutpat porttitor magna, non finibus neque cursus id.
+
+
 def index(request):
     return render(request, 'oc_lettings_site/index.html')
