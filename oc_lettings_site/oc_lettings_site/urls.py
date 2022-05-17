b0f489d4ef91dfc44786e2f6a4f171468f3a4f04
JulienC-Dev <julien.cormier@yahoo.fr>
2022-05-17 20:13:13 +0200

    add sentry

--- oc_lettings_site/oc_lettings_site/urls.py
+++ oc_lettings_site/oc_lettings_site/urls.py
@@ -3,9 +3,14 @@ from django.contrib import admin
 from . import views
 
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
+    path('sentry-debug/', trigger_error),
     path('lettings/', include('lettings.urls')),
     path('profiles/', include('profiles.urls')),
 ]
+
