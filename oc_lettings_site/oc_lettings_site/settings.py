517888c401f918b1d212c3373762be16589ee20d
JulienC-Dev <julien.cormier@yahoo.fr>
2022-05-17 13:11:53 +0200

    modif

--- oc_lettings_site/oc_lettings_site/settings.py
+++ oc_lettings_site/oc_lettings_site/settings.py
@@ -13,8 +13,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY_DJANGO")
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ["DEBUG_DJANGO"] == 'TRUE'
-DEBUG = False
+DEBUG = os.environ["DEBUG_DJANGO"] == 'TRUE'
+# DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 # Application definition
