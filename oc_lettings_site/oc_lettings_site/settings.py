eee30efa39a43af6646d61b5715d4e24dfb662ac
JulienC-Dev <julien.cormier@yahoo.fr>
2022-05-18 13:03:55 +0200

    modification ROOT projects

--- oc_lettings_site/oc_lettings_site/settings.py
+++ oc_lettings_site/oc_lettings_site/settings.py
@@ -112,8 +112,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles'),
+]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 sentry_sdk.init(
     dsn=os.environ.get('SENTRY_DSN'),
