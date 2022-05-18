c7a073d8275998c70100d478d795a9823e572115
JulienC-Dev <julien.cormier@yahoo.fr>
2022-05-18 13:14:22 +0200

    update ROOT

--- oc_lettings_site/oc_lettings_site/settings.py
+++ oc_lettings_site/oc_lettings_site/settings.py
@@ -114,12 +114,12 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
-]
+print(STATIC_URL)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+print(STATIC_ROOT)
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'staticfiles'),
+# ]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 sentry_sdk.init(
